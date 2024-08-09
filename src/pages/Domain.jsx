d62b14f640d7b6d46cb39fa8393aa9db5f107030
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2024-08-09 19:42:23 +0530

    Merge branch 'master' of https://github.com/xterious/hkton24

--- src/pages/Domain.jsx
+++ src/pages/Domain.jsx
@@ -4,17 +4,17 @@ const Domain = () => {
         {
             title:"Website Development",
             image:"https://th.bing.com/th?id=OIP.xi_VY54V535hztHz11VTyQHaFL&w=298&h=209&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2",
-            description:"Web development involves creating and maintaining websites and web applications. Participants will use technologies like HTML, CSS, JavaScript, and various frameworks to build interactive and responsive web experiences."
+            description:"Build dynamic and responsive web applications that push the boundaries of innovation. Showcase your skills in front-end, back-end, or full-stack development."
         },
         {
             title:"Mobile App Development",
             image:"https://th.bing.com/th?id=OIP.O4b29g448P1Js7pElwG6-AHaFC&w=303&h=206&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2",
-            description:"Web development involves creating and maintaining websites and web applications. Participants will use technologies like HTML, CSS, JavaScript, and various frameworks to build interactive and responsive web experiences."
+            description:"Create intuitive and impactful mobile apps that enhance user experiences. Whether native or cross-platform, bring your app ideas to life."
         },
         {
             title:"Artificial Intelligence",
             image:"https://th.bing.com/th?id=OIP.KwPFjO2CC4CXhWNkNkpmLAHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2",
-            description:"Web development involves creating and maintaining websites and web applications. Participants will use technologies like HTML, CSS, JavaScript, and various frameworks to build interactive and responsive web experiences."
+            description:"Develop intelligent systems using the latest in AI and machine learning. Innovate solutions that can transform industries and shape the future."
         }
         ];
   return (
