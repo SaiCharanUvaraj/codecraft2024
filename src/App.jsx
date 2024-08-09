76b551cf1afb1e9ec2406660386d02db2c0563c4
Kamalesh <kamalesh2023@gmail.com>
2024-08-09 13:57:58 +0530

    Changes v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import "./App.css";
+import ParticlesComponent from "./components/Particles";
+
 function App() {
   return (
-    <>
-      <div className="">NIger anaa website</div>
-    </>
+    <div className="App">
+      <ParticlesComponent id="particles" />
+      <h1>Particles are so fun!</h1>
+    </div>
   );
 }
 
