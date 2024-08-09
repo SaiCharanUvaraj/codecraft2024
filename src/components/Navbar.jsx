e337e402f24f35aa2810ad2baf206f4076394377
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2024-08-09 19:50:31 +0530

    navbar updation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import Sidebar from "./Sidebar"
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-5 fixed bg-white/5 backdrop-blur-sm rounded-b-3xl w-full">
+    <div className="flex justify-between items-center p-5 fixed bg-white/5 backdrop-blur-xl border-b-2 border-white/15 shadow-xl shadow-white/5 rounded-b-3xl w-full">
         <p className="text-5xl font-bold text-[#5F85DB]">Hackathon Logo</p>
         <Sidebar/>
     </div>
