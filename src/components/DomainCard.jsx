c222e2f0f3dbd2d9842005992b2953a73e4d0be3
Koushik <koushikbabu003@gmail.com>
2024-08-11 12:21:35 +0530

    koushik trying to avoid merge conflicts

--- src/components/DomainCard.jsx
+++ src/components/DomainCard.jsx
@@ -1,16 +1,27 @@
-const DomainCard = (props) =>{
-    return (
-          <div className="transition-all duration-500 basis-1/3 bg-[#31363F] rounded-lg hover:shadow-lg hover:shadow-[#5F85DB]">
-              <div className="text-center p-3 text-2xl font-bold text-black bg-[#5F85DB] rounded-t-lg">
-                  {props.title}
-              </div>
-              <div className="flex flex-col justify-center items-center p-4">
-                  <img className="w-full h-56 rounded-xl mt-2 mb-2 pr-2 pl-2" src={props.image}/>
-                  <p className="text-white text-lg text-center pr-2 pl-2">
-                      {props.description}
-                  </p>
-              </div>
-          </div>
-        )
-    }
+import Lottie from 'lottie-react';
+import { Card } from "flowbite-react";
+
+
+export function DomainCard(props) {
+  return (
+    <Card
+  className="max-w-sm flex flex-col items-center"
+>
+  <div className="w-full h-48 flex justify-center">
+    <Lottie animationData={props.animationData} loop={true} />
+  </div>
+  <div className="w-full h-10 flex flex-col justify-center">
+    <div className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      {props.title}
+    </div>
+  </div>
+  <div className="w-full h-24 flex flex-col justify-center">
+    <div className="font-normal text-gray-700 dark:text-gray-400">
+      {props.description}
+    </div>
+  </div>
+</Card>
+  )
+}
+
 export default DomainCard
--- src/pages/Domain.jsx
+++ src/pages/Domain.jsx
@@ -1,27 +1,32 @@
 import DomainCard from "../components/DomainCard";
+
+import aniData1 from '../assets/Animation - 1723345948359.json';
+import aniData2 from '../assets/Animation - 1723346036691.json';
+import aniData3 from '../assets/Animation - 1723346095382.json';
+
 const Domain = () => {
     const cards=[
         {
+            animationData:aniData1,
             title:"Website Development",
-            image:"https://th.bing.com/th?id=OIP.xi_VY54V535hztHz11VTyQHaFL&w=298&h=209&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2",
-            description:"Build dynamic and responsive web applications that push the boundaries of innovation. Showcase your skills in front-end, back-end, or full-stack development."
+            description:"Build dynamic and responsive web applications. Showcase your skills in front-end, back-end, or full-stack development."
         },
         {
+            animationData:aniData2,
             title:"Mobile App Development",
-            image:"https://th.bing.com/th?id=OIP.O4b29g448P1Js7pElwG6-AHaFC&w=303&h=206&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2",
-            description:"Create intuitive and impactful mobile apps that enhance user experiences. Whether native or cross-platform, bring your app ideas to life."
+            description:"Create intuitive and impactful mobile apps that enhance user experiences. Whether native or cross-platform."
         },
         {
+            animationData:aniData3,
             title:"Artificial Intelligence",
-            image:"https://th.bing.com/th?id=OIP.KwPFjO2CC4CXhWNkNkpmLAHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2",
-            description:"Develop intelligent systems using the latest in AI and machine learning. Innovate solutions that can transform industries and shape the future."
+            description:"Develop intelligent systems using the latest in AI and machine learning."
         }
         ];
   return (
     <div className="grid place-items-center pt-28 w-full" id="domain">
         <p className="text-center text-3xl font-bold text-white pb-5">Domains Available</p>
         <div className="grid md:grid-cols-3 place-items-center gap-5 md:px-10 px-2">
-            {cards.map((card,index) => <DomainCard key={index} title={card.title} description={card.description} image={card.image}/> ) }
+            {cards.map((card,index) => <DomainCard key={index} title={card.title} description={card.description} animationData={card.animationData}/> ) }
         </div>
     </div>
   )
