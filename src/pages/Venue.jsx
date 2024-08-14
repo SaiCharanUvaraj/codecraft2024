17a18c6cad61b8f9732d1178511e89f614c53af7
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2024-08-14 14:08:05 +0530

    updated venue

--- src/pages/Venue.jsx
+++ src/pages/Venue.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Venue = () => {
   return (
-    <div className='text-white pt-24'>
-        <p className='text-center font-semibold text-6xl'>Venue</p>
+    <div className='flex justify-center items-cente'>
+      <div className='text-white mt-24 border-[#AAAAAA] border-2 rounded-xl md:w-2/4 w-full p-10 pt-2 bg-white/5 backdrop-blur-2xl border-b-2 border-white/15'>
+        <p className='text-center font-semibold text-6xl pb-10'>Venue</p>
+        <p className='text-center font-semibold text-3xl pb-5'>Gamma Hall, 3rd Floor, Abdul Kalam Library Block</p>
+        <p className='text-center font-semibold text-2xl pb-5'>Sri Sairam Engineering College</p>
+        <p className='text-center font-semibold text-1xl pb-5'>Sai Leo Nagar, West Tambaram, Chennai- 600044.</p>
+      </div>
     </div>
   )
 }
