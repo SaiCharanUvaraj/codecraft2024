4204cc1316e4c27e9fa8ef2070cccafe867ce846
Guru Sanjay R K <gurucoolmsd@gmail.com>
2024-08-16 18:17:44 +0530

    select-none

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@
 
   function App() {
     return (
-      <div className='hideSc'>
+      <div className='select-none'>
         <Navbar />
         <div className='p-4'>
           <ParticlesComponent id="particles" />
